1768053484d2fe0cae6577a8db4ce29163d067ae
Yuhan Liu <yuhanl@umich.edu>
2016-11-28 20:46:08 -0500

    attack 1

---
+++ OneHitKill/Assets/Scripts/Player_Attack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Attack : MonoBehaviour {
+
+    public GameObject sword;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(sword, transform.position + transform.forward, transform.rotation);
+        }
+    }
+}
